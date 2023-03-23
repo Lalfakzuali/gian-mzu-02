9c5a2684d9de8dee3ad6a385ddfea15fc6808110
Lalfakzuali <lalfakzualihauzel@gmail.com>
2023-03-23 15:08:58 +0530

    O1-hello-r file added

---
+++ 01-hello-r.R
@@ -0,0 +1,15 @@
+#01-hello-r.R
+
+my_var <- c(a='apple', b='boy', c='cat', d='desktop')
+
+#To get the 3rd variable i.e, cat:
+my_var[3]
+
+my_var['c']
+
+#To get: cat,boy,apple,desktop:
+my_var[c(3,2,1,4)]
+
+my_var[c('c','b','a','d')]
+
+
